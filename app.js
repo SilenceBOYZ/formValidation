09f1bccde3c0f57f74b359bb6232c55dce459537
SilenceBOYZ <nguyenthanhtri230799@gmail.com>
2023-07-06 09:51:47 +0700

    Phần code này cần phải xem lại phần them required cho email

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function validator(options) {
   // Tạo hàm validate để gán đoạn code kiểm tra lỗi
   // Hàm thực hiện validate
   function validate(inputElement, rule) {
-    var errorElement = inputElement.parentElement.querySelector('.form-message');
+    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
     // Lấy value: từ inputElement.value
     // láy hàm test từ rules.test
     var errorMessage = rule.test(inputElement.value);
@@ -47,22 +47,49 @@ function validator(options) {
 // Nguyên tắc của các rules
 // 1. Khi có lỗi => trả ra messae lỗi
 // 2. khi hợp lệ =. không trả ra cái gì cả (undefined)
-validator.isRequired = function (selector) {
+validator.isRequired = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
       // Hàm trim loại bỏ các dấu cách của hai bên đầu hoặc cuối
-      return value.trim() ? undefined : "Vui lòng nhập trường này";
+      return value.trim() ? undefined : message || "Vui lòng nhập trường này";
     }
   };
 }
 
-validator.isEmail = function (selector) {
+validator.isEmail = function (selector, message) {
   return {
     selector: selector,
     test: function (value) {
       var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      return regex.test(value) ? undefined : "Trường này phải là email";
+      return regex.test(value) ? undefined : message || "Trường này phải là email";
     }
   };
+}
+
+validator.minLength = function (selector, min, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} ký tự `;
+    }
+  };
+}
+
+validator.isSamePassword = function (selector, message) {
+  return {
+    selector: selector,
+    test: function (value) {
+      return value ? undefined : message || "Vui lòng nhập trường này";
+    }
+  };
+}
+
+validator.isConfirmed = function (selector, getConfirmValue, message) {
+  return {
+    selector: selector,
+    test: function (value){
+      return value === getConfirmValue() ? undefined : message || "Giá trị nhập vào không chính xác";
+    }
+  }
 }
